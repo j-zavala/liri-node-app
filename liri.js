f31d8e7267990989bd70dacf026e5638b332c2c1
Johnny Zavala <jzavalafgz@gmail.com>
2018-08-21 12:19:15 -0400

    imported key.js to liri.js and listed project variables

--- liri.js
+++ liri.js
@@ -1,1 +1,15 @@
+// using .env to hide keys
 require("dotenv").config();
+
+// project variables
+let keys = require('./keys.js');
+let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+let fs = require('fs');
+let SpotifyAPI = require('node-spotify-api');
+let request = require('request');
+let twiiter = require('twitter')
+
+
+
+
