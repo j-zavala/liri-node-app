9709f4823bfe29adadb0cb69b9f98d795fbc7691
Johnny Zavala <jzavalafgz@gmail.com>
2018-08-24 22:40:59 -0400

    added spotify function

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 // project variables
 let keys = require('./keys.js');
-let SpotifyAPI = require('node-spotify-api');
+let Spotify = require('node-spotify-api');
 let request = require('request');
 let Twitter = require('twitter');
 let fs = require('fs');
@@ -45,11 +45,68 @@ let getTweets = function(username) {
 };
 
 function spotifySong(song) {
-    spotify.search({type: 'track', query: 'song'}, function(error, data) {
-        console.log(JSON.parse(data));
-    })
+
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+        for (let i=0; i<data.tracks.items.length; i++) {
+            let songData = data.tracks.items[i];
+            let artist = "Artist: " + songData.artists[0].name;
+            let song = "Song: " + songData.name;
+            let link = "Preview URL: " + songData.preview_url;
+            let album = "Album: " + songData.album.name;
+            let divider = "------------------------------------------------"
+            //get artist
+            console.log(artist);
+            // get song name
+            console.log(song);
+            //get spotify preview link
+            console.log(link);
+            //get album name
+            console.log(album);
+            console.log(divider);
+
+            // adds text to log.txt
+            fs.appendFile('log.txt', "\n"  + artist + "\n", 'utf8', function(err) {
+                if (err) {
+                    throw(err);
+                }
+            });
+            fs.appendFile('log.txt', song + "\n", 'utf8', function(err) {
+                if (err) {
+                    throw(err);
+                }
+            });
+            fs.appendFile('log.txt', link + "\n", 'utf8', function(err) {
+                if (err) {
+                    throw(err);
+                }
+            });
+            fs.appendFile('log.txt', album + "\n", 'utf8', function(err) {
+                if (err) {
+                    throw(err);
+                }
+            });
+            fs.appendFile('log.txt', divider, 'utf8', function(err) {
+                if (err) {
+                    throw(err);
+                }
+            });
+        }
+        
+      });
 }
 
 
+
+// function spotifySong(song) {
+//     spotify.search({type: 'track', query: 'song'}, function(error, data) {
+//         console.log(JSON.parse(data));
+//     })
+// }
+
+
 // getTweets("JohnSte78206501");
-spotifySong("You got the love");
+spotifySong('All the Small Things');
