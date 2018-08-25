72737652cefaf0b994b3898079ee706a927df3ad
Johnny Zavala <jzavalafgz@gmail.com>
2018-08-24 22:00:29 -0400

    added tweets function

--- liri.js
+++ liri.js
@@ -3,13 +3,53 @@ require("dotenv").config();
 
 // project variables
 let keys = require('./keys.js');
-let spotify = new Spotify(keys.spotify);
-let client = new Twitter(keys.twitter);
-let fs = require('fs');
 let SpotifyAPI = require('node-spotify-api');
 let request = require('request');
-let twiiter = require('twitter')
+let Twitter = require('twitter');
+let fs = require('fs');
+let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+let command = process.argv[2];
+
+let getTweets = function(username) {
+    // Display last 20 tweets;
+    let screenName = {screen_name: username};
+    client.get('statuses/user_timeline', screenName, function(error, tweets, response) {
+
+        // console.log("tweets: ", tweets);
+        
+        if (!error) {
+            for (let i=0; i<tweets.length; i++) {
+                let date = tweets[i].created_at;
+                let string = "\n@JohnSte78206501: " + tweets[i].text + "\nCreated at: " + date.substring(0, 19);
+                let divider = "\n------------------------------------------------";
+                console.log(string);
+                console.log(divider);
+
+                //adds text to log file
+                fs.appendFile('log.txt', string, 'utf8', function(err) {
+                    if(err) {
+                        throw(err);
+                    }
+                });
+                fs.appendFile('log.txt', divider, 'utf8', function(err){
+                    if(err) {
+                        throw(err);
+                    }
+                });
+            }
+        } else {
+            console.log('Error occurred');
+        }
+    });
+};
+
+function spotifySong(song) {
+    spotify.search({type: 'track', query: 'song'}, function(error, data) {
+        console.log(JSON.parse(data));
+    })
+}
 
 
-
-
+// getTweets("JohnSte78206501");
+spotifySong("You got the love");
